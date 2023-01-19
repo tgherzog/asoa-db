fea551cad9d3688849eb1ef447b03be8f21b7397
Tim Herzog <timgherzog@gmail.com>
2023-01-19 02:10:19 -0500

    Code for user-specific logins based on hull, boat name and email. Might build on this in the future...

--- app.py
+++ app.py
@@ -19,10 +19,6 @@ config = {
 
 config['db_lastmod'] = xl.load_workbook(filename=config['db_path']).properties.modified
 
-# set "asoa_access_mode=members" as an environment variable to enable privileged access
-# to member-only database content and features
-config['access_mode'] = os.environ.get('asoa_access_mode', 'public')
-
 app = flask.Flask(__name__)
 # if being run via gunicorn, make us proxy-aware (assume Apache)
 if os.environ.get('_', '').endswith('gunicorn'):
@@ -33,6 +29,28 @@ assets.append_path('assets/scss')
 assets.url_expire = False   # disable dynamic CSS loading
 scss = Bundle('custom.scss', filters='pyscss', output='css/custom.css')
 assets.register('scss_site', scss)
+
+# secret-key.txt is used to enable user sessions. The key must be secret so it is
+# not kept in the repository. Generate one like this:
+#
+#    python -c 'import secrets; print(secrets.token_hex())' > secret-key.txt
+#
+# For more details:
+# https://flask.palletsprojects.com/en/2.2.x/quickstart/#sessions
+with open('secret-key.txt', 'rb') as fd:
+    app.secret_key = fd.read()
+
+# use server-side sessions; that way we can reset them easily
+from flask_session import Session
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+
+def init_session():
+    '''Call immediately upon an entry point to authenticate and load session info
+    '''
+
+    config['access_mode'] = 'members' if flask.session.get('member') else 'public'
 
 
 @app.template_filter('markdown')
@@ -65,16 +83,53 @@ def context_processor():
 @app.route('/')
 def app_start():
 
+  init_session()
   return flask.render_template('start.html')
+
+
+@app.route('/login', methods=['POST'])
+def login_validate():
+
+
+    form = {}
+    okay = True
+    for key in ['hull', 'email', 'boat_name']:
+        form[key] = flask.request.form[key].rstrip()
+        if not form[key]:
+            okay = False
+            break
+
+    if okay:
+        boat = db_load(form=form)
+        if boat:
+            flask.session['member'] = boat['hull']
+            flask.flash('You are signed in')
+        else:  
+            okay = False
+
+    if not okay:
+        flask.flash('Invalid login', 'error')
+
+    return flask.redirect('/')
+
+@app.route('/logout')
+def logout():
+
+    flask.session.clear()
+    flask.flash('You are signed out')
+    return flask.redirect('/')
+
 
 @app.route('/list')
 def app_list():
 
+    init_session()
     return flask.render_template('list.html', page_title='List of All Known Seabreezes', boats=db_load())
 
 @app.route('/detail/<hull>')
 def app_detail(hull):
 
+    init_session()
     boat = db_load(hull)
     if boat:
         # we also pass the original hull number in case of error
@@ -85,6 +140,7 @@ def app_detail(hull):
 @app.route('/search')
 def app_search():
     
+    init_session()
     q = flask.request.args.get('q')
     if q:
         try:
@@ -99,12 +155,17 @@ def app_search():
 
     return flask.redirect(flask.url_for('app_list'))
 
+@app.route('/download/members')
 def download_member_file():
 
     wb = xl.Workbook()
     ws = wb[wb.sheetnames[0]]
     ws.title = 'members'
     hdrFont = Font(bold=True)
+
+    init_session()
+    if config['access_mode'] != 'members':
+        flask.abort(403)
 
     fields = ['hull', 'boat_name', 'status', 'sailnum', 'rig', 'owner_name', 'acquired', 'address1', 'address2', 'phone', 'email', 'berth', 'latest_info', 'epitaph']
     for n in range(len(fields)):
@@ -155,11 +216,7 @@ def download_member_file():
             download_name=filename, as_attachment=True)
 
 
-if config['access_mode'] == 'members':
-    app.add_url_rule('/download/members', view_func=download_member_file)
-
-
-def db_load(id=None, q=None, raw=False):
+def db_load(id=None, q=None, raw=False, form=None):
     '''Loads the database from a spreadsheet.
       
        id:      hull number. If defined, the function returns a single record (dict), otherwise
@@ -167,6 +224,7 @@ def db_load(id=None, q=None, raw=False):
 
        q:       search text to filter results. Else return all boats
 
+       raw:     return typed values - don't convert to strings
     '''
 
     def filter_boats(item):
@@ -220,6 +278,10 @@ def db_load(id=None, q=None, raw=False):
 
         boat['date'] = fmt_date(boat['date'])
 
+        if form:
+            if boat['hull'] == form['hull'] and boat['boat_name'] == form['boat_name'] and boat['email'] == form['email']:
+                return boat
+
         hull = boat['hull']
         if boat_db.get(hull):
             # Prior record exists: append owner to front and merge more recent fields
@@ -236,7 +298,9 @@ def db_load(id=None, q=None, raw=False):
 
             boat_db[hull] = boat
 
-    if id:
+    if form:
+        return False
+    elif id:
         return boat_db.get(id, {})
     elif q:
         q = q.lower()
